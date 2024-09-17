6ea9aeeffad1a7aa00ae5018972f0fd2189e4a18
xarlizard <charkrios@gmail.com>
2024-09-17 13:08:14 +0200

    showcase show button toggle addedd

--- src/js/onclick_row_toggle.js
+++ src/js/onclick_row_toggle.js
@@ -1,11 +1,10 @@
 var c = 0; //iteration counter
-var foldable1 = document.getElementById('foldable1');
-var foldable2 = document.getElementById('foldable2');
-var showMore = document.getElementById('showMore');
-function show_row() {
+
+function toggle_row() {
     var foldable1 = document.getElementById('foldable1');
     var foldable2 = document.getElementById('foldable2');
     var showMore = document.getElementById('showMore');
+    var showLess = document.getElementById('showLess');
 
     if (c == 0) {
       foldable1.style.display = "flex";
@@ -13,6 +12,13 @@ function show_row() {
     } else if (c == 1) {
       foldable2.style.display = "flex";
       showMore.style.display = "none"
+      showLess.style.display = "flex"
       c++;
+    } else if (c == 2) {
+      foldable1.style.display = "none";
+      foldable2.style.display = "none";
+      c = 0;
+      showMore.style.display = "flex"
+      showLess.style.display = "none"
     }
-  }
+}
