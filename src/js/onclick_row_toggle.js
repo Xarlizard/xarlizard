be3ab4e2e6e94731eb747ea5880bc6119350f678
xarlizard <charkrios@gmail.com>
2024-10-15 18:03:10 +0200

    Updated foldable function - now swaps visibility

--- src/js/onclick_row_toggle.js
+++ src/js/onclick_row_toggle.js
@@ -1,20 +1,27 @@
 var c = 0; //iteration counter
 
 function toggle_row() {
+    var foldable0 = document.getElementById('foldable0');
     var foldable1 = document.getElementById('foldable1');
     var foldable2 = document.getElementById('foldable2');
     var showMore = document.getElementById('showMore');
     var showLess = document.getElementById('showLess');
 
     if (c == 0) {
+      foldable0.style.display = "none";
       foldable1.style.display = "flex";
+      foldable1.style.flexDirection = "row";
       c++;  
     } else if (c == 1) {
+      foldable1.style.display = "none";
       foldable2.style.display = "flex";
+      foldable2.style.flexDirection = "row";
       showMore.style.display = "none"
       showLess.style.display = "flex"
       c++;
     } else if (c == 2) {
+      foldable0.style.display = "flex";
+      foldable0.style.flexDirection = "row";
       foldable1.style.display = "none";
       foldable2.style.display = "none";
       c = 0;
