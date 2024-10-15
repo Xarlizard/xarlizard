dc40474bfe33e1a46dead7bb44c4ee981c456512
xarlizard <charkrios@gmail.com>
2024-10-15 17:09:07 +0200

    Updated ✅

--- src/js/onclick_show_img.js
+++
@@ -1,8 +0,0 @@
-function show_image(image) {
-    var x = image.querySelector('.values');
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
